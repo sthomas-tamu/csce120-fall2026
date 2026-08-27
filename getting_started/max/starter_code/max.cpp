cfab182dc5da25e3faf6df548d4d111d7edc94cf
sthomas-tamu <sthomas@cse.tamu.edu>
2026-08-27 11:59:35 -0500

    updating indention

--- getting_started/max/starter_code/max.cpp
+++ getting_started/max/starter_code/max.cpp
@@ -2,12 +2,12 @@
 #include <iostream>
 #include <limits>
 
-int main () {
-    std::cout << "Enter 3 numbers." << std::endl;
+int main() {
+  std::cout << "Enter 3 numbers." << std::endl;
 
-    int value = 0;
-    std::cin >> value;
-    std::cout << "Read in " << value << std::endl;
+  int value = 0;
+  std::cin >> value;
+  std::cout << "Read in " << value << std::endl;
 
-    return 0;
+  return 0;
 }
