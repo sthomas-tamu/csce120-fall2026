98650da245297cfdcf43fb71be57b87c377b6c60
sthomas-tamu <sthomas@cse.tamu.edu>
2026-08-27 11:52:31 -0500

    starter code for reading in 3 numbers and reporting the greatest

---
+++ getting_started/max/starter_code/max.cpp
@@ -0,0 +1,13 @@
+// find the max of 3 numbers
+#include <iostream>
+#include <limits>
+
+int main () {
+    std::cout << "Enter 3 numbers." << std::endl;
+
+    int value = 0;
+    std::cin >> value;
+    std::cout << "Read in " << value << std::endl;
+
+    return 0;
+}
