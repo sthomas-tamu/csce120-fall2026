71eb70a54d1016371db29da1df7974de4e65aebd
sthomas-tamu <sthomas@cse.tamu.edu>
2026-08-31 09:00:02 -0500

    starter code for preferene example

---
+++ struct/preferences/starter_code/preference.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+// TODO(@Student): Create Preference struct to store
+//                 name and if prefers morning or evening
+
+int main() {
+  // TODO(@Student): Instantiate a Preference object for each person
+  //                 Print them out
+
+  return 0;
+}
