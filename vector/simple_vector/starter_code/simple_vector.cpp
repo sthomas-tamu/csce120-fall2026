3fd20bd7ddd73cd567c950ac1bf8a2ea9cb07681
sthomas-tamu <sthomas@cse.tamu.edu>
2026-08-31 08:53:21 -0500

    simple vector example

---
+++ vector/simple_vector/starter_code/simple_vector.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+void PrintVector(std::string label, const std::vector<int> v) {
+  std::cout << "The " << label << " vector elements:" << std::endl;
+  for (unsigned int i = 0; i < v.size(); ++i) {
+    std::cout << label << "[" << i << "] = " << v.at(i) << std::endl;
+  }
+  std::cout << std::endl;
+}
+
+// TODO(@Student): Write a function SumVector that returns the sum of all the elements
+
+
+int main() {
+  // create an empty vector
+  std::vector<int> empty_vector;
+  PrintVector("empty", empty_vector);
+
+  // initialize a vector with braces
+  std::vector<int> odds = {1, 3, 5, 7, 9};
+  PrintVector("odds", odds);
+
+  // initialize a vector with 5 elements set to 0
+  std::vector<int> evens(5, 0);
+
+  // update all values
+  for (unsigned int i = 0; i < evens.size(); ++i) {
+    evens.at(i) = i*2;
+  }
+
+  // traverse and print values
+  PrintVector("evens", evens);
+
+  // access a single element
+  std::cout << "front: " << evens.front() << std::endl;
+  std::cout << "back: " << evens.back() << std::endl;
+  std::cout << "at index 1: " << evens.at(1) << std::endl;
+  // std::cout << "at index -1: " << evens.at(-1) << std::endl;
+
+  // add contents to the vector
+  evens.push_back(10);
+  evens.push_back(12);
+  PrintVector("updated evens", evens);
+  // what is the new size?
+
+  // sum the values
+  // TODO(@Student): Uncomment code once SumVector is written
+  /*
+  std::cout << "The sum of the evens vector is "
+    << SumVector(evens) << std::endl;
+  */
+}
