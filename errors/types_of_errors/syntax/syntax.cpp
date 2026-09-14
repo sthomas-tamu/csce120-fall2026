57c72b7cadaf79cf4cca712d3a2e507efd3e9a32
akronnie55 <akronnie55@gmail.com>
2026-09-14 16:18:51 -0500

    09/14

--- errors/types_of_errors/syntax/syntax.cpp
+++ errors/types_of_errors/syntax/syntax.cpp
@@ -3,12 +3,12 @@
 // 4 errors
 
 int main() {
-  unsignd int age = 0;
+  unsigned int age = 0; // unsignd int age = 0;
   std::cout << "Enter your age: ";
-  cin >> age;
+  std::cin >> age; // cin >> age;
 
-  if (12 < age < 20) {
-    std::cout >> "You are a teenager." >> std::endl;
+  if (12 < age & age < 20) { // if (12 < age < 20) {
+    std::cout << "You are a teenager." << std::endl; // std::cout >> "You are a teenager." >> std::endl;
   }
 
   return 0;
